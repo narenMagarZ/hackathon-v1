f877a24b79fd7d0fda91f9d9e60b4a2db9048e29
narenmagarz <narenmagarz98@gmail.com>
2023-10-09 15:38:11 +0545

    feat: Add function that handle global error and return response

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ const app = express()
 
 app.set('view engine','ejs')
 
+app.use(express.static('public'))
+
 app.use('/api',apiRouter)
 app.use('/',viewRouter)
-app.use(express.static('public'))
+
+app.use((err,req,res,next)=>{
+     const code = err.code
+     const message = err.message
+     return res.status(code).json({
+          error:message
+     })
+})
 export default app
