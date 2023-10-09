e4d3368bd4799d57686eb995920d72aaa6a2565f
krishna panthi <krishnapanthi94@gmail.com>
2023-10-09 22:58:18 +0545

    adding initial footer and animation for navbar

--- public/scripts/myscript.js
+++ public/scripts/myscript.js
@@ -1,3 +1,5 @@
+
+
 document.getElementById("toggleButton").addEventListener("click", function() {
     var notificationPanel = document.getElementById("notification_panel");
     if (notificationPanel.classList.contains("hidden")) {
@@ -8,8 +10,43 @@ document.getElementById("toggleButton").addEventListener("click", function() {
         notificationPanel.style.right = "-350px"; 
     }
 });
+
+/* for nav animation */
+const navbar = document.querySelector('.navbar');
+const intermediateContainer = document.querySelector('.Container_for_datacard');
+let prevScrollPos = window.pageYOffset;
+let isNavbarHidden = false;
+/* ---------------- */
+
 window.addEventListener("scroll", function() {
     var notificationPanel = document.getElementById("notification_panel");
     notificationPanel.classList.add("hidden");
-    notificationPanel.style.right = "-350px"; // Hide by translating back outside the right corner
-});
+    notificationPanel.style.right = "-350px";
+
+    /* for nav bar animation */
+    const currentScrollPos = window.pageYOffset;
+  
+    if (currentScrollPos > intermediateContainer.offsetTop) {
+      if (!isNavbarHidden) {
+        navbar.style.transform = 'scale(0)';
+        dropDownMenu.style.transform = 'scale(0)';
+        isNavbarHidden = true;
+      }
+    } else {
+      if (isNavbarHidden) {
+        navbar.style.transform = 'scale(1)';
+        dropDownMenu.style.transform = 'scale(1)';
+        isNavbarHidden = false;
+      }
+    }
+  
+    if (currentScrollPos < prevScrollPos) {
+      navbar.style.transform = 'scale(1)';
+      dropDownMenu.style.transform = 'scale(1)';
+      isNavbarHidden = false;
+    }
+  
+    prevScrollPos = currentScrollPos;
+  }
+);
+
