64bcf07b6bb72fa7db2250bcf284caea876d2511
narenmagarz <narenmagarz98@gmail.com>
2023-10-09 15:34:12 +0545

    organize and export modules in main entry file

---
+++ utils/index.js
@@ -0,0 +1,11 @@
+
+import appError from "./app-error.js";
+import getSuggestedCrops from "./get-suggested-crops.js";
+import getClimate from "./get-climate.js";
+import getWeatherFromCode from "./get-weather-from-code.js";
+export {
+     appError,
+     getSuggestedCrops,
+     getClimate,
+     getWeatherFromCode
+}
