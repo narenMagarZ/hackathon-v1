2af2f2de07902cf2c0f1b1f7ab34e17f1270a88d
narenmagarz <narenmagarz98@gmail.com>
2023-10-09 22:18:35 +0545

    Add a controller to handle signin

---
+++ controller/signin.js
@@ -0,0 +1,37 @@
+import { appError, compareHash, generateJwt } from "../utils/index.js"
+import joi from 'joi'
+import UserService from '../services/user-service.js'
+async function signin(req,res,next){
+     const {error,value}=joi.object({
+          phoneNumber:joi.string().required(),
+          password:joi.string().required()
+     }).validate(req.body)
+     if(error){
+          return next(new appError(400,error.message))
+     }
+     const {phoneNumber,password} = value
+     try{
+          const user = await new UserService().getUserByPhoneNumber(phoneNumber)
+          if(!user){
+               return next(new appError(404,"User not found"))
+          }
+          const isPasswordMatched = await compareHash(password,user.password)
+          if(isPasswordMatched){
+               res.cookie("token",generateJwt({
+                    id:user.id
+               }))
+               return res.status(200).json({
+                    status:"success",
+                    message:"Signin successfully"
+               })
+          }else {
+               return next(new appError(401,"Password does not match"))
+          }
+     }
+     catch(err){
+          console.error(err)
+          return next(new appError(500,err.message))
+     }
+}
+
+export default signin
