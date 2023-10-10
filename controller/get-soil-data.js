17d7cb19e74df8f52f620aad09d3d3d99893b615
narenmagarz <narenmagarz98@gmail.com>
2023-10-10 22:12:15 +0545

    check weather the suggested crops are cached or not

--- controller/get-soil-data.js
+++ controller/get-soil-data.js
@@ -1,17 +1,19 @@
 import joi from 'joi'
-import { getSuggestedCrops,getClimate, getWeatherFromCode } from '../utils/index.js'
+import { getSuggestedCrops,getClimate, getWeatherFromCode, appError } from '../utils/index.js'
 import axios from 'axios'
+import redis from '../config/redis.js'
 async function getSoilData(req,res,next){
      const {error,value}=joi.object({
           lat:joi.number().required(),
           lng:joi.number().required()
      }).validate(req.query)
+     console.log(req.query)
      if(error){
-          return next()
+          return next(new appError(400,error.message))
      }
      try{
           const {lat,lng}=value
-          const response = await axios.get(`https://soil.narc.gov.np/soil/api/soildata?lat=${lat}&lon=${lng}`)
+          const response = await axios.get(`https://soil.narc.gov.np/soil/api/soildata?lat=${parseInt(lat)}&lon=${parseInt(lng)}`)
           const soilData = response.data
           let climate = await getClimate(lat,lng)
           const weather = getWeatherFromCode(climate.weathercode)
@@ -21,10 +23,16 @@ async function getSoilData(req,res,next){
                is_day:undefined,
                weathercode:undefined
           }
-          const suggestedCrops = await getSuggestedCrops(`Acts as a farmer, suggest a crops which are suitable to harvest for this ${soilData}.Response must be in a json format, include only name field of crops.Note:no need to mention introduction and conclusion only the name of crops`)
+          let suggestedCrops = await redis.get(`suggested-crop:lat:${lat},lng:${lng}`)
+          let isCached = true
+          if(!suggestedCrops){
+               isCached = false
+               suggestedCrops = await getSuggestedCrops(`Acts as a farmer, suggest a crops which are suitable to harvest for this ${soilData}.Response must be in a json format, include only name field of crops.Note:no need to mention introduction and conclusion only the name of crops`)
+               await redis.set(`suggested-crop:lat:${lat},lng:${lng}`,JSON.stringify(suggestedCrops))
+          }
           return res.status(200).json({
                soilData,
-               suggestedCrops,
+               suggestedCrops:isCached?JSON.parse(suggestedCrops):suggestedCrops,
                climate
           })
      }
