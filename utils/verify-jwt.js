59c6be126fe97242ea5b50e8fc93dd749930dfe6
narenmagarz <narenmagarz98@gmail.com>
2023-10-10 10:02:14 +0545

    add a env variable

--- utils/verify-jwt.js
+++ utils/verify-jwt.js
@@ -1,6 +1,9 @@
 import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
 
+dotenv.config()
 function verifyJwt(token){
+     console.log(token,process.env.JWT_SECRET_KEY)
      return jwt.verify(token,process.env.JWT_SECRET_KEY)
 }
 
