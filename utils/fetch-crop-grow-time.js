f12c1d3c7c21db5a290b5a7dd46f0c446e69148f
narenmagarz <narenmagarz98@gmail.com>
2023-10-10 10:03:46 +0545

    add a function that fetches the crop harvest period

--- utils/fetch-crop-grow-time.js
+++ utils/fetch-crop-grow-time.js
@@ -4,11 +4,12 @@ async function fetchCropGrowTime(crop){
      const response = await openAi.chat.completions.create({
           messages:[{
                role:"user",
-               content:`Acts as a pro farmer, return the time to grow ${crop} in a json format.Note: The json data should only contain period field, the period must be exact, int type and represents as month`
+               content:`Acts as a pro farmer, return the time to grow ${crop} in a json format.Note: The json data should only contain period field, the period must be exact, int type and represents as month.In addition, no need to add introductio and conclusion.`
           }],
           model:"gpt-3.5-turbo"
      })
      console.log(response.choices[0].message.content)
+     return JSON.parse(response.choices[0].message.content).period
 }
 
-fetchCropGrowTime("maize")
+export default fetchCropGrowTime
