d158629a09e3cea7a3f1d25d5a303057cc7e13a5
narenmagarz <narenmagarz98@gmail.com>
2023-10-10 10:09:18 +0545

    add a new keyword

--- models/yield-crops.js
+++ models/yield-crops.js
@@ -2,7 +2,7 @@
 import mongoose from "mongoose";
 
 
-const yieldCropsSchema = mongoose.Schema({
+const yieldCropsSchema = new mongoose.Schema({
      name:{
           type:String,
           required:true
