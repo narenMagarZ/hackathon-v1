d0ba6acd4f22fa360fc5b3d708435c156dfe3a09
narenmagarz <narenmagarz98@gmail.com>
2023-10-10 09:55:58 +0545

    Add a middleware function that authenticate the user

---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+import { appError, verifyJwt } from "../utils/index.js"
+import UserService from '../services/user-service.js'
+
+
+async function auth(req,res,next){
+     try{
+          const {token} = req.signedCookies
+          const jwtPayload = verifyJwt(token)
+          console.log(jwtPayload)
+          const {id} = jwtPayload
+          const user = await new UserService().getUser(id)
+          if(user){
+               req.user = {
+                    id:user.id,
+                    name:user.firstName+' '+user.lastName
+               }
+               next()
+          }
+          else return next(new appError(401,"Unauthorized"))
+     }
+     catch(err){
+          console.error(err)
+          return next(new appError(500,err.message))
+     }
+}
+
+export default auth
