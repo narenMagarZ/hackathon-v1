f9b6c6c64400d24cad79f6e919d43738f7f781ef
narenmagarz <narenmagarz98@gmail.com>
2023-10-10 10:04:44 +0545

    add a signin, signup, etc router

--- router/apiRouter.js
+++ router/apiRouter.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import {getCropProcedure, getSoilData} from '../controller/index.js'
+import auth from '../middleware/auth.js'
+import {cropHarvestingProcedure, getSoilData, signup, signin, yieldCrop,getYieldedCrops} from '../controller/index.js'
 const apiRouter = express.Router()
 
 
 
 apiRouter.get('/soildata',getSoilData)
 
-apiRouter.get('/procedure',getCropProcedure)
+apiRouter.get('/procedure',cropHarvestingProcedure)
+apiRouter.post('/auth/signup',signup)
+apiRouter.post('/auth/signin',signin)
+apiRouter.post('/yieldcrop',auth,yieldCrop)
 export default apiRouter
